0bb732d3fa87eed51af64f02c2a4550ac4be0305
ian-dungan <iandungan@me.com>
2025-12-13 17:04:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 // =======================
 // SUPABASE SETUP
 // =======================
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://depvgmvmqapfxjwkkhas.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcHZnbXZtcWFwZnhqd2traGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5NzkzNzgsImV4cCI6MjA4MDU1NTM3OH0.WLkWVbp86aVDnrWRMb-y4gHmEOs9sRpTwvT8hTmqHC0";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Username-only auth (fake email)
